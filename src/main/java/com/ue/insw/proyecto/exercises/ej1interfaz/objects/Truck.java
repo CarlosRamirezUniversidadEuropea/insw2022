5871ce30d9efc11a9ffc7673a39e7d09352e85d9
Ana Esteban <ana.esteban@balanceit.org>
2022-11-08 21:02:07 +0100

    Commit Truck

--- src/main/java/com/ue/insw/proyecto/exercises/ej1interfaz/objects/Truck.java
+++ src/main/java/com/ue/insw/proyecto/exercises/ej1interfaz/objects/Truck.java
@@ -1,6 +1,16 @@
 package com.ue.insw.proyecto.exercises.ej1interfaz.objects;
 
+import java.math.BigDecimal;
+import com.ue.insw.proyecto.exercises.ej1interfaz.interfaces.Cleanable;
+
 //todo extender de Vehicle
 //todo implements Cleanable
-public class Truck {
+public class Truck extends Vehicle implements Cleanable{
+    private BigDecimal price;
+    public Truck(BigDecimal price) {
+        super(price);
+    }
+    public void clean() {
+        System.out.println("Bicicleta limpiándose");
+    }
 }
