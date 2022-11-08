93b7865d5869d0afb0f079a32e5dfe29174d7c66
Ana Esteban <ana.esteban@balanceit.org>
2022-11-08 20:58:25 +0100

    Commit Bicycle

--- src/main/java/com/ue/insw/proyecto/exercises/ej1interfaz/objects/Bicycle.java
+++ src/main/java/com/ue/insw/proyecto/exercises/ej1interfaz/objects/Bicycle.java
@@ -1,9 +1,16 @@
 package com.ue.insw.proyecto.exercises.ej1interfaz.objects;
 
+import java.math.BigDecimal;
 import com.ue.insw.proyecto.exercises.ej1interfaz.interfaces.Cleanable;
 
 //todo extender de Vehicle
 //todo implements Cleanable
-public class Bicycle extends Vehicle implements Cleanable {
-
+public class Bicycle extends Vehicle implements Cleanable{
+    private BigDecimal price;
+    public Bicycle(BigDecimal price) {
+        super(price);
+    }
+    public void clean() {
+        System.out.println("Bicicleta limpiándose");
+    }
 }
