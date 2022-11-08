49010ba02b71aef38c9c339bc943327635f003a4
IgnacioJodarVega <paranacho1@gmail.com>
2022-11-08 22:22:13 +0100

    Envío mi ejercicio. Me avisa de algunas advertencias que he intentado solucionar pero no lo he logrado

--- src/main/java/com/ue/insw/proyecto/exercises/ej1interfaz/Main.java
+++ src/main/java/com/ue/insw/proyecto/exercises/ej1interfaz/Main.java
@@ -2,7 +2,9 @@ package com.ue.insw.proyecto.exercises.ej1interfaz;
 
 import com.ue.insw.proyecto.exercises.ej1interfaz.enumerates.Brand;
 import com.ue.insw.proyecto.exercises.ej1interfaz.enumerates.Color;
+import com.ue.insw.proyecto.exercises.ej1interfaz.objects.Bicycle;
 import com.ue.insw.proyecto.exercises.ej1interfaz.objects.Car;
+import com.ue.insw.proyecto.exercises.ej1interfaz.objects.Truck;
 
 import java.math.BigDecimal;
 
@@ -14,6 +16,14 @@ public class Main {
         System.out.println(peugeot.getBrand());
         System.out.println(peugeot.getColor());
         System.out.println(peugeot.toString());
+        peugeot.clean();
+
+
+        Truck citroen = new Truck(Brand.CITROEN, Color.GREEN, 200, new BigDecimal(9000));
+        System.out.println(citroen.getPrice());
+        System.out.println(citroen.getBrand("Citroen"));
+        System.out.println(citroen.getColor("Green"));
+        System.out.println(citroen.toString());
         peugeot.clean();
     }
 }
--- src/main/java/com/ue/insw/proyecto/exercises/ej1interfaz/objects/Truck.java
+++ src/main/java/com/ue/insw/proyecto/exercises/ej1interfaz/objects/Truck.java
@@ -1,6 +1,140 @@
 package com.ue.insw.proyecto.exercises.ej1interfaz.objects;
 
-//todo extender de Vehicle
-//todo implements Cleanable
-public class Truck {
+import com.ue.insw.proyecto.exercises.ej1interfaz.enumerates.Brand;
+import com.ue.insw.proyecto.exercises.ej1interfaz.enumerates.Color;
+import com.ue.insw.proyecto.exercises.ej1interfaz.enumerates.Gasoline;
+import com.ue.insw.proyecto.exercises.ej1interfaz.enumerates.Status;
+import com.ue.insw.proyecto.exercises.ej1interfaz.interfaces.Cleanable;
+
+import java.math.BigDecimal;
+
+import static com.ue.insw.proyecto.exercises.ej1interfaz.enumerates.Status.ON;
+import static com.ue.insw.proyecto.exercises.ej1interfaz.enumerates.Status.STOPED;
+
+public class Truck extends Vehicle implements Cleanable {
+
+    private Brand brand;
+    private Color color;
+    private int maxSpeed;
+    private int speed;
+    private Status status;
+
+    public Truck(Brand brand, Color color, int maxSpeed, BigDecimal price) {
+        super(price);
+        this.brand = brand;
+        this.color = color;
+        this.maxSpeed = maxSpeed;
+        this.speed = 0;
+        this.status = STOPED;
+    }
+
+    public Brand getBrand(String citroen) {
+        return brand;
+    }
+
+    public Color getColor(String green) {
+        return color;
+    }
+
+    public Truck(BigDecimal price) {
+        super(price);
+    }
+
+    //todo javadoc
+    public void on() {
+        this.status = ON;
+    }
+
+    //todo javadoc
+    public void stop() {
+        this.speed = 0;
+        this.status = STOPED;
+    }
+
+    //todo javadoc
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public void fillCombustible(Gasoline gasoline, int liters) {
+
+        }
+
+    private void fillCar() {
+        fillCombustible();
+    }
+
+    private void fillCombustible() {                //Siendo la capacidad máxima del camión 50 l
+        int capacidadMax = 0;
+        System.out.println("Cuanta gasolina quiere echar?");
+        //Leemos la variable que contenga la cantidad de gasolina. En este caso: capacidadMax.
+        if(capacidadMax == 50){
+            System.out.println("Se ha alcanzado la máxima capacidad de gasolina. ");
+        }else if(capacidadMax > 50){
+            System.out.println("Se ha sobrepasado la máxima capacidad de gasolina");
+        }else{
+            System.out.println("Rellenando gasolina");
+        }
+    }
+
+
+    private void startDriving (int speed, int time) {
+        startDriving(speed, time);
+    }
+
+    @Override
+    public void clean() {
+        System.out.println("El camión se está limpiando");
+    }
+
+    public Brand getBrand() {
+        return brand;
+    }
+
+    public void setBrand(Brand brand) {
+        this.brand = brand;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public int getMaxSpeed() {
+        return maxSpeed;
+    }
+
+    //todo la velocidad tiene que ser un numero positivo, modificar método, encapsulamiento
+    public void setMaxSpeed(int maxSpeed) {
+        this.maxSpeed = maxSpeed;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "brand=" + brand +
+                ", color=" + color +
+                ", maxSpeed=" + maxSpeed +
+                ", speed=" + speed +
+                ", status=" + status +
+                '}';
+    }
 }
+
+
+
