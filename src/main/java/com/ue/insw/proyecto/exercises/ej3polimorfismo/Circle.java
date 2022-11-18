3fa7ed6797a0c477179e5dae7009a8d79a2e8a2e
Mario Gutiérrez <marioguti033@gmail.com>
2022-11-18 17:15:32 +0100

    Algunos cambios EJ 2 y 4 y Ej 3 terminado

--- src/main/java/com/ue/insw/proyecto/exercises/ej3polimorfismo/Circle.java
+++ src/main/java/com/ue/insw/proyecto/exercises/ej3polimorfismo/Circle.java
@@ -1,5 +1,30 @@
 package com.ue.insw.proyecto.exercises.ej3polimorfismo;
+import java.lang.Math.*;
 
-//todo extends shape
-public class Circle {
+public class Circle extends Shape{
+    double radio;
+    @Override
+    public double calculatePerimeter() {
+        return 2 * radio * Math.PI;
+    }
+
+    @Override
+    public double calculateArea() {
+        return Math.PI * radio * radio;
+    }
+
+    @Override
+    public String toString() {
+        return "Circle{" +
+                "radio=" + radio +
+                '}';
+    }
+
+    public double getRadio() {
+        return radio;
+    }
+
+    public void setRadio(double radio) {
+        this.radio = radio;
+    }
 }
--- src/main/java/com/ue/insw/proyecto/exercises/ej3polimorfismo/Rectangle.java
+++ src/main/java/com/ue/insw/proyecto/exercises/ej3polimorfismo/Rectangle.java
@@ -1,5 +1,31 @@
 package com.ue.insw.proyecto.exercises.ej3polimorfismo;
 
-//todo extends shape
-public class Rectangle {
+public class Rectangle extends Shape{
+    double base;
+    double altura;
+    @Override
+    public double calculatePerimeter() {
+        return 2*(base + altura);
+    }
+
+    @Override
+    public double calculateArea() {
+        return base * altura;
+    }
+
+    @Override
+    public String toString() {
+        return "Rectangle{" +
+                "base=" + base +
+                ", altura=" + altura +
+                '}';
+    }
+
+    public double getBase() {
+        return base;
+    }
+
+    public void setBase(double base) {
+        this.base = base;
+    }
 }
