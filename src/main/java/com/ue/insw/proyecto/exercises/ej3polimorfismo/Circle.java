3dcfd4f3011cebaa1816dea1e1e72160b9f0cb0f
PelayoVT <pelayo.pvt@gmail.com>
2022-11-24 20:36:57 +0100

    Ejercicio 3

--- src/main/java/com/ue/insw/proyecto/exercises/ej3polimorfismo/Circle.java
+++ src/main/java/com/ue/insw/proyecto/exercises/ej3polimorfismo/Circle.java
@@ -1,5 +1,35 @@
 package com.ue.insw.proyecto.exercises.ej3polimorfismo;
 
 //todo extends shape
-public class Circle {
+public class Circle extends Shape {
+
+    private double radio;
+    static final double PI = 3.141592653589793;
+
+    public Circle(double radio) {
+        this.radio = radio;
+    }
+
+    public double getRadio() {
+        return radio;
+    }
+
+    public void setRadio(double radio) {
+        this.radio = radio;
+    }
+
+    @Override
+    public double calculatePerimeter() {
+        return 0;
+    }
+
+    @Override
+    public double calculateArea() {
+        return PI*(radio*radio);
+    }
+
+    @Override
+    public String toString() {
+        return null;
+    }
 }
--- src/main/java/com/ue/insw/proyecto/exercises/ej3polimorfismo/Rectangle.java
+++ src/main/java/com/ue/insw/proyecto/exercises/ej3polimorfismo/Rectangle.java
@@ -1,5 +1,55 @@
 package com.ue.insw.proyecto.exercises.ej3polimorfismo;
 
 //todo extends shape
-public class Rectangle {
+public class Rectangle extends Shape {
+
+    private double lado1;
+    private double lado2;
+
+    private String color;
+
+    public Rectangle(double lado1, double lado2) {
+        this.lado1 = lado1;
+        this.lado2 = lado2;
+        this.color = "white";
+    }
+
+    public double getLado1() {
+        return lado1;
+    }
+
+    public void setLado1(double lado1) {
+        this.lado1 = lado1;
+    }
+
+    public double getLado2() {
+        return lado2;
+    }
+
+    public void setLado2(double lado2) {
+        this.lado2 = lado2;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public double calculatePerimeter() {
+        return 2*(lado1+lado2);
+    }
+
+    @Override
+    public double calculateArea() {
+        return lado1*lado2;
+    }
+
+    @Override
+    public String toString() {
+        return "Este rectangulo tiene una base de " + lado1 + " y una altura de " + lado2;
+    }
 }
