05b63c850390799e6e26607562b18fead5b4bb83
anaestg <ana.esteban@balanceit.org>
2022-11-24 18:46:26 +0100

    Extends e Implements

--- src/main/java/com/ue/insw/proyecto/exercises/ej2herencia/Dog.java
+++ src/main/java/com/ue/insw/proyecto/exercises/ej2herencia/Dog.java
@@ -1,5 +1,16 @@
 package com.ue.insw.proyecto.exercises.ej2herencia;
 
-public class Dog {
+import com.ue.insw.proyecto.exercises.ej2herencia.Animal;
+import com.ue.insw.proyecto.exercises.ej2herencia.Speak;
+
+public class Dog extends Animal implements Speak{
+
     // todo complete extends Animal implements speak, override methods
+    public Dog(String raza, String nombre, int edad) {
+        super(raza, nombre, edad);
+    }
+
+    public void speak() {
+        System.out.println("Woof");
+    }
 }
