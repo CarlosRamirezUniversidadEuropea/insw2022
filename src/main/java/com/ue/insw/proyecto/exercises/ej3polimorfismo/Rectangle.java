4bc89c8698f51bca113ed347fa7fbb62967a61b6
Alvaro Jose Huamani Lara <alvaro2002711@gmail.com>
2022-11-27 02:35:46 +0100

    Ejercicio , modificado_rectangle

--- src/main/java/com/ue/insw/proyecto/exercises/ej3polimorfismo/Rectangle.java
+++ src/main/java/com/ue/insw/proyecto/exercises/ej3polimorfismo/Rectangle.java
@@ -1,5 +1,80 @@
 package com.ue.insw.proyecto.exercises.ej3polimorfismo;
 
+
+
 //todo extends shape
-public class Rectangle {
+public class Rectangle extends Shape {
+    private String color ;
+
+    private int ladoLargo;
+
+    private int ladoCorto;
+
+    public abstract double calculetPerimeter;
+
+    public abstract double calculetArea;
+
+
+
+
+    public Rectangle (String color, int ladoLargo, int ladoCorto){
+        this.color=color;
+        this.ladoLargo=ladoLargo;
+        this.ladoCorto=ladoCorto;
+
+
+    }
+
+    public String getColor(){
+        return Color;
+    }
+
+    public void setColor(String color){
+        this.color=color;
+    }
+
+    public int getLadoLargo(){
+        return ladoLargo;
+    }
+
+    public void setLadoLargo(int ladoLargo) {
+        this.ladoLargo = ladoLargo;
+    }
+
+    public int getLadoCorto() {
+        return ladoCorto;
+    }
+
+    public void setLadoCorto(int ladoCorto) {
+        this.ladoCorto = ladoCorto;
+    }
+
+    public double getCalculetPerimeter() {
+        return ladoLargo * 2 + ladoCorto * 2 ;
+    }
+
+    public void setCalculetPerimeter(double calculetPerimeter) {
+        this.calculetPerimeter = calculetPerimeter;
+    }
+
+    public void setCalculetArea(double calculetArea) {
+        this.calculetArea = calculetArea;
+    }
+
+    public double getCalculetArea() {
+        return ladoCorto * ladoLargo;
+    }
+
+    @Override
+    public String toString() {
+        return "Rectangle{" +
+                "color=" + color
+                "ladoLargo= " ladoLargo
+                "ladoCorto=" + ladoCorto
+                "calculetPerimeter=" + calculetPerimeter
+                "caluletArea= " + calculetArea
+                "}";
+    }
+
+
 }
