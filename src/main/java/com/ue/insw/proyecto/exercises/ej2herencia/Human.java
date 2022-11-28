468fb55bc531e844f745d1f8cbbc737d13d48ae8
Manu <manuaraba@gmail.com>
2022-11-28 02:26:00 +0100

    Main ej 2 terminado

--- src/main/java/com/ue/insw/proyecto/exercises/ej2herencia/Human.java
+++ src/main/java/com/ue/insw/proyecto/exercises/ej2herencia/Human.java
@@ -3,12 +3,21 @@ package com.ue.insw.proyecto.exercises.ej2herencia;
 // todo extends animal, implements speak ?
 public class Human extends Animal implements Speak {
 
-    public Human(String raza, String nombre){
-        super(raza,nombre);
+    public Human(String raza, String nombre,int edad){
+        super(raza,nombre,edad);
 
+    }
+
+    public String toString(){
+        try {
+            return "Humano{ Nombre= " + getNombre() + ", Raza= " + getRaza() + ", Edad= "+getEdad()+"}";
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
     @Override
     public void speak() {
         System.out.println("Hola mundo");
     }
+
 }
