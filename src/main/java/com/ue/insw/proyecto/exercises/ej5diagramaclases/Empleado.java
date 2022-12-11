0e97b19dd2f0db4d2d161035bcf88086a0c7a8e7
anaestg <ana.esteban@balanceit.org>
2022-12-11 19:44:38 +0100

    Calcular salario neto

--- src/main/java/com/ue/insw/proyecto/exercises/ej5diagramaclases/Empleado.java
+++ src/main/java/com/ue/insw/proyecto/exercises/ej5diagramaclases/Empleado.java
@@ -3,13 +3,16 @@ import java.util.ArrayList;
 
 public class Empleado extends Persona{
 
-    double sueldo_bruto;
+    double sueldo_bruto, impuestos;
 
     void mostrar(){
         System.out.println("Sueldo Bruto: " +sueldo_bruto);
+        calcular_salario_neto(sueldo_bruto, impuestos);
     }
 
-    void calcular_salario_neto(){
-
+    void calcular_salario_neto(double sb, double imp){
+        double salario_neto;
+        salario_neto = sb - imp;
+        System.out.println("Salario neto: " +salario_neto);
     }
 }
