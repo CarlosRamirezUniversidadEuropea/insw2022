8f6501327b8d48364f0d9169963dcd9c3c0b2451
Christian Jonathan Yang Gonzalez <christianj_yang@icloud.com>
2022-12-12 22:03:40 +0100

    Ejercicio 4 completo y comenzar ejercicio 5

--- src/main/java/com/ue/insw/proyecto/exercises/ej4/Pizza.java
+++ src/main/java/com/ue/insw/proyecto/exercises/ej4/Pizza.java
@@ -8,11 +8,22 @@ public class Pizza {
     private List<String> ingredients;
     private BakeStyle bakeStyle;
 
-    public Pizza(String base, List<String> ingredients) {
-
+    public Pizza(String base, List<String> ingredients, BakeStyle bakeStyle) {
+        this.base = base;
+        this.ingredients = ingredients;
+        this.bakeStyle = bakeStyle;
     }
 
     public void bake() {
         bakeStyle.bake(this);
+    }
+
+    @Override
+    public String toString() {
+        return "Pizza{" +
+                "base='" + base + '\'' +
+                ", ingredients=" + ingredients +
+                ", bakeStyle=" + bakeStyle +
+                '}';
     }
 }
---
+++ src/main/java/com/ue/insw/proyecto/exercises/ej5diagramaclases/Cliente.java
@@ -0,0 +1,4 @@
+package com.ue.insw.proyecto.exercises.ej5diagramaclases;
+
+public class Cliente {
+}
