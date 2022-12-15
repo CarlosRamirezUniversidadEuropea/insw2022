6d01abdac68571046f61306b638857f4841afc62
manuel araujo baño <manuaraba@gmail.com>
2022-12-15 21:47:55 +0100

    ACTUALIZADO Json y el de los animales

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,12 @@
 module com.ue.insw.proyecto {
     requires javafx.controls;
     requires javafx.fxml;
+    requires com.google.gson;
+    opens com.ue.insw.proyecto.exercises.json to com.google.gson;
+    requires com.fasterxml.jackson.databind;
+
+
+
 
     exports com.ue.insw.proyecto.controller;
     opens com.ue.insw.proyecto.controller to javafx.fxml;
